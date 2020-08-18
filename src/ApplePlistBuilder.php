46aa775c406bbe28cdaeefddccfe5c3c094ff9d0
buchwasa <17461354+buchwasa@users.noreply.github.com>
2020-08-17 23:48:00 -0400

    PSR-12 fixes

--- src/ApplePlistBuilder.php
+++ src/ApplePlistBuilder.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace appleplistbuilder;
@@ -60,7 +61,7 @@ final class ApplePlistBuilder
         return $this->metadataKind;
     }
 
-    public function toPlist() : SimpleXMLElement
+    public function toPlist(): SimpleXMLElement
     {
         return simplexml_load_string(
             <<<XML
